4d613d748e673062363da421653355d797a1954f
Cristian Torres <ctorres@uesebe.cl>
2019-01-26 16:28:14 -0300

    Initial Commit

---
+++ 02-dices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02-dices
+//
+//  Created by Cristian Torres on 1/26/19.
+//  Copyright © 2019 Cristian Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
